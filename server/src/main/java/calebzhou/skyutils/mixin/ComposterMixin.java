c83891b3204e877291a6360b0a0c807f2e52e87b
calebz <portableryanc69@outlook.com>
2022-09-04 07:54:28 +0800

    234

--- server/src/main/java/calebzhou/skyutils/mixin/ComposterMixin.java
+++ server/src/main/java/calebzhou/skyutils/mixin/ComposterMixin.java
@@ -1,22 +1,30 @@
 package calebzhou.skyutils.mixin;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ComposterBlock.class)
 public abstract class ComposterMixin {
   //堆肥桶倒出来一个草方块，instead of 骨粉
-        @Redirect(method = "extractProduce",
-        at=@At(value = "NEW",target = "net.minecraft.world.item.ItemStack")
-        )
-        private static ItemStack extractGrassBlockFromComposterBlock(ItemLike itemLike){
-          return new ItemStack(Items.GRASS_BLOCK);
+        @Inject(method = "extractProduce",
+        at=@At(value = "INVOKE",target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z")
+        ,locals = LocalCapture.CAPTURE_FAILSOFT)
+        private static void extractGrassBlockFromComposterBlock(BlockState blockState, Level level, BlockPos blockPos, CallbackInfoReturnable<BlockState> cir, float f, double d, double e, double g, ItemEntity itemEntityBoneMeal){
+            ItemEntity itemEntity = new ItemEntity(level, (double)blockPos.getX() + d, (double)blockPos.getY() + e, (double)blockPos.getZ() + g, new ItemStack(Items.GRASS_BLOCK));
+            itemEntity.setDefaultPickUpDelay();
+            level.addFreshEntity(itemEntity);
         }
 
   /*
